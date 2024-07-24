b8c19e7960b785ff84c883e0b8bc1e5e23ca7740
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-07-24 14:52:54 +0530

    Learned about passing parameters through url
    practicing GET API request

--- Learning/04-natours/app.js
+++ Learning/04-natours/app.js
@@ -25,6 +25,19 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', result: tours.length, data: { tours: tours } });
 });
 
+app.get('/api/v1/tours/:id/:what?',(req,res) => {
+  console.log(req.params);
+  const id = req.params.id * 1; // This is the way to get the parameters from the url and using the '?' this sign we can also declare optional url parameters // and multiplying it with 1 automatically converts the String to int if the value of String is 1
+  const tour = tours.find(el => el.id == id);
+  if (!tour){
+    return res.status(404).json({status: 'fail',data: 'No tour found'});
+  }
+
+  res.status(200).send({status:'success',data:{
+    tour
+  }})
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
