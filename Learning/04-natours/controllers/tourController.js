df241a7bfe497532c038b08f04b1d64136b2b2b0
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-07-25 13:48:05 +0530

    Param middleware

--- Learning/04-natours/controllers/tourController.js
+++ Learning/04-natours/controllers/tourController.js
@@ -1,11 +1,22 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
-  
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
 // 2) Route Handlers (Tours)
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is ${val}`)
+  if (req.params.id * 1 > tours.length) {
+    return res.status(400).send({
+      status: 'fail',
+        message: 'Invalid tour ID',
+      
+    });
+  }
+  next();
+};
+
 exports.getALlTours = (req, res) => {
   console.log(`request time: ${req.requestTime}`);
   res.status(200).json({
@@ -17,12 +28,8 @@ exports.getALlTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
   const id = req.params.id * 1; // This is the way to get the parameters from the url and using the '?' this sign we can also declare optional url parameters // and multiplying it with 1 automatically converts the String to int if the value of String is 1
   const tour = tours.find((el) => el.id == id);
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', data: 'No tour found' });
-  }
 
   res.status(200).send({
     status: 'success',
@@ -54,12 +61,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(400).send({ status: 'fail', data: null });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -69,17 +70,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(400).send({
-      status: 'fail',
-      data: {
-        message: 'Invalid tour ID',
-      },
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: {
