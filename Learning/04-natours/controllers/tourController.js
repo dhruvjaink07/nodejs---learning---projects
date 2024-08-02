5db0e26d7a446fd52bc8fb72968b8a67b7a662f6
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-08-02 17:57:52 +0530

    Advance Filtering of gte, gt, lte, lt

--- Learning/04-natours/controllers/tourController.js
+++ Learning/04-natours/controllers/tourController.js
@@ -3,11 +3,19 @@ const Tour = require('./../models/tourModel');
 exports.getALlTours = async (req, res) => {
 try{
   // BUILDING QUERY
+  // 1) Filtering
   const queryObj = {...req.query};
   const excludedFields = ['page','sort','limit','fields'];
   excludedFields.forEach(el=> delete queryObj[el]);
 
-  const query = Tour.find(queryObj);
+  // 2) Advance Filtering
+  // Very Useful for getting the exact data on user's preference 
+  // So on the API we can do comparisions like greater than equal, greater, lesser, lesser then equal and much more
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  console.log(JSON.parse(queryStr));
+
+  const query = Tour.find(JSON.parse(queryStr));
   // const allTours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
 
   // EXECUTING QUERY
