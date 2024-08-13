f7f1368fda5b25b3a8ddd41b36b4f6a90e5fe5e6
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-08-13 21:34:57 +0530

    Document Middlewaree

--- Learning/04-natours/models/tourModel.js
+++ Learning/04-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // Tour Scheme
 const tourSchema = new mongoose.Schema({
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must Have a name'],
     unique: true,
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration']
@@ -30,8 +32,9 @@ const tourSchema = new mongoose.Schema({
   },
   ratingsAverage: {
     type: Number,
+    default: 4.5
   },
-  ratingsQuantity: Number,
+  ratingsQuantity: {type: Number, default:0},
   imageCover: {
     type: String,
     required: [true, 'A tour must have a Image Cover'],
@@ -57,6 +60,22 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
   return this.duration / 7;
 })
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower : true});
+    next();
+});
+
+// tourSchema.pre('save',function(next){
+//   console.log("Will Save Document");
+//   next();
+// })
+
+// tourSchema.post('save', function(doc, next){
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
